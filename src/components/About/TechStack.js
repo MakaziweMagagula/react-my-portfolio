22b6416ba21551cb136ede881fc1f6445d6ae3cb
MakaziweMagagula <makaziwem23@gmail.com>
2021-12-20 12:12:38 +0200

    Adding a Tooltip

--- src/components/About/TechStack.js
+++ src/components/About/TechStack.js
@@ -1,4 +1,5 @@
 import React from "react";
+import ReactTooltip from 'react-tooltip';
 import { Col, Row } from "react-bootstrap";
 import {
   SiCsharp,
@@ -13,8 +14,10 @@ import {
 function Techstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiCsharp />
+       
+       <Col xs={4} md={2} className="tech-icons">
+       <p data-tip="C# (C-Sharp) is a programming language developed by Microsoft that runs on the .NET Framework"> <SiCsharp /> </p>
+       <ReactTooltip />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiJava />
